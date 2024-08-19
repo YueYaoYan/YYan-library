9106540fb0399d8fd3c3980c72c28701413e1089
Yao <86668901+YueYaoYan@users.noreply.github.com>
2024-08-19 11:09:51 +1000

    Completed task 1-5

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import "./assets/main.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-// import './style.css'
-import "bootstrap/dist/css/bootstrap.min.css";
-import PrimeVue from "primevue/config";
-import Aura from "@primevue/themes/aura";
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
-const app = createApp(App);
-app.use(PrimeVue, { theme: { preset: Aura } });
+const app = createApp(App)
+app.use(PrimeVue, { theme: { preset: Aura } })
+app.use(router)
 
-createApp(App).mount("#app");
+app.mount('#app')
